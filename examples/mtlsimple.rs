62c18cf7422997806ba3ce75aa7ce8c372a148d0
Adam Nemecek <adamnemecek@gmail.com>
2020-08-04 15:03:29 -0700

    mtlsimple::text

--- examples/mtlsimple.rs
+++ examples/mtlsimple.rs
@@ -316,8 +316,9 @@ fn main() {
                 // draw_rounded_rect(&mut canvas, 100.0, 100.0, 40.0, 40.0, 5.0, 1.0);
                 draw_rect(&mut canvas, 100.0, 100.0, 40.0, 40.0);
                 draw_rect(&mut canvas, 200.0, 200.0, 40.0, 40.0);
-                draw_colorwheel(&mut canvas, 200.0, 200.0, 200.0, 200.0, 5.0);
-                draw_image(&mut canvas, images[0], 300.0, 300.0);
+                draw_colorwheel(&mut canvas, 400.0, 200.0, 200.0, 200.0, 5.0);
+                draw_image(&mut canvas, images[0], 5.0, 300.0);
+                draw_text(&mut canvas, &fonts, "title", 5.0, 200.0, 100.0, 100.0);
                 // draw_rounded_rect(&mut canvas,300.0, 100.0, 40.0, 40.0, 5.0, 1.0);
                 // render_clear_rect(&mut );
 
@@ -356,6 +357,17 @@ fn draw_image<T: Renderer>(canvas: &mut Canvas<T>, image: ImageId, x: f32, y: f3
     let mut path = Path::new();
     path.rounded_rect(x, y, w as _, h as _, 5.0);
     canvas.fill_path(&mut path, img_paint);
+    canvas.restore();
+}
+
+fn draw_text<T: Renderer>(canvas: &mut Canvas<T>, fonts: &Fonts, title: &str, x: f32, y: f32, w: f32, h: f32) {
+    canvas.save();
+    let mut text_paint = Paint::color(Color::rgba(200, 200, 200, 255));
+    text_paint.set_font_size(16.0);
+    text_paint.set_font(&[fonts.regular]);
+    text_paint.set_text_align(Align::Left);
+    text_paint.set_text_baseline(Baseline::Middle);
+    let _ = canvas.fill_text(x + h, y + h * 0.5, title, text_paint);
     canvas.restore();
 }
 
