d9bf2b707b6f68a4297cb538bc5714aedebf45a2
Adam Nemecek <adamnemecek@gmail.com>
2020-08-12 10:43:46 -0700

    moved gpuvecext

--- src/renderer/mtl.rs
+++ src/renderer/mtl.rs
@@ -19,23 +19,9 @@ mod stencil_texture;
 use stencil_texture::StencilTexture;
 
 mod mtl_ext;
-pub use mtl_ext::{generate_mipmaps, MtlTextureExt};
+pub use mtl_ext::{generate_mipmaps, MtlTextureExt, GPUVecExt};
 
-pub trait GPUVecExt {
-    fn extend_with_triange_fan_indices_cw(&mut self, start: u32, count: u32) -> usize;
-}
 
-impl GPUVecExt for GPUVec<u32> {
-    fn extend_with_triange_fan_indices_cw(&mut self, start: u32, count: u32) -> usize {
-        let mut added = 0;
-        for index in 1..(count - 1) {
-            self.extend_from_slice(&[start, start + index, start + index + 1]);
-            added += 3;
-        }
-
-        added
-    }
-}
 
 /// Cached render pipeline states
 #[allow(dead_code)]
@@ -128,83 +114,7 @@ struct MtlCompositeOperationState {
 //     include!(concat!(env!("OUT_DIR"), "/gl_bindings.rs"));
 // }
 
-/// Creates an indidex buffer which can be used to "fake" triangle fans
-/// Based on pathfinder.
-/// https://www.gamedev.net/forums/topic/643945-how-to-generate-a-triangle-fan-index-list-for-a-circle-shape/
 
-fn triangle_fan_indices_cw(start: u32, len: u32) -> Vec<u32> {
-    let mut indices: Vec<u32> = vec![];
-    for index in 1..(len - 1) {
-        indices.extend_from_slice(&[start, start + index, start + index + 1]);
-    }
-
-    indices
-}
-fn triangle_fan_indices_ccw(start: u32, len: u32) -> Vec<u32> {
-    let mut indices: Vec<u32> = vec![];
-    for index in 1..(len - 1) {
-        indices.extend_from_slice(&[start, start + index + 1, start + index]);
-    }
-
-    indices
-}
-
-// from https://github.com/OpenSmalltalk/opensmalltalk-vm/blob/4ee8bb6e7960e5776558f0baca10daee7ec5d653/platforms/iOS/plugins/B3DAcceleratorPlugin/sqMetalRenderer.m#L718
-// unsigned int triangleCount = vertexCount - 2;
-// unsigned int renderIndexCount = triangleCount*3;
-// id<MTLBuffer> indexBuffer = [device newBufferWithLength: renderIndexCount*4 options: MTLResourceStorageModeManaged];
-
-// // Set the triangle fan indices.
-// unsigned int *destIndices = (unsigned int *)indexBuffer.contents;
-// for(unsigned int i = 2; i < vertexCount; ++i) {
-//     destIndices[0] = 0;
-//     destIndices[1] = i - 1;
-//     destIndices[2] = i;
-//     destIndices += 3;
-// }
-
-fn triangle_fan_indices2(device: &metal::DeviceRef, start: u32, len: u32) -> GPUVec<u32> {
-    let triangle_len = len - 2;
-    let index_len = 3 * triangle_len;
-    let mut vec = GPUVec::<u32>::with_capacity(device, index_len as usize);
-
-    // let mut indices: Vec<u32> = vec![];
-    for index in 2..(len) {
-        vec.extend_from_slice(&[start, start + index - 1, start + index]);
-    }
-
-    vec
-}
-
-// fn triangle_fan_indices3(
-//     start: u32,
-//     len: u32
-// ) -> Vec<u32> {
-//     let triangle_len = len - 2;
-//     let index_len = 3 * triangle_len;
-//     let mut vec = Vec::<u32>::with_capacity(index_len as);
-
-//     // let mut indices: Vec<u32> = vec![];
-//     for index in 2..(len) {
-//         vec.extend_from_slice(&[start, start + index - 1, start + index]);
-//     }
-
-//     vec
-// }
-/// expects buffer to be able to allocate vertices
-fn triangle_fan_indices_ext(start: u32, len: usize, buf: &mut GPUVec<u32>) {
-    // let mut indices: Vec<u32> = vec![];
-    let invariant = buf.capacity();
-    // for index in start..(start + len as u32 - 1) {
-    //     buf.extend_from_slice(&[start, index, index + 1]);
-    // }
-
-    for index in start..(start + len as u32 - 1) {
-        buf.extend_from_slice(&[start, index, index + 1]);
-    }
-
-    assert!(invariant == buf.capacity());
-}
 
 // fn prepare_pipeline_state<'a>(
 //     device: &DeviceRef,
@@ -1407,29 +1317,6 @@ impl Renderer for Mtl {
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::triangle_fan_indices_cw;
-    // use super::{}
-    // use metalgear::GPUVec;
-    // use super::GPUVecExt;
-
-    // #[test]
-    // fn test_triangle_fan_indices() {
-
-    // }
-
-    #[test]
-    fn test_triangle_fan_indices_cw() {
-        let expected: Vec<u32> = vec![0, 1, 2, 0, 2, 3, 0, 3, 4];
-        let result = triangle_fan_indices_cw(0, 5);
-        assert!(expected == result);
-
-        let expected: Vec<u32> = vec![2, 3, 4, 2, 4, 5, 2, 5, 6];
-        let result = triangle_fan_indices_cw(2, 5);
-        assert!(expected == result);
-    }
-}
 
 // pub struct SolidTexture {
 //     w: usize,
--- src/renderer/mtl/mtl_ext.rs
+++ src/renderer/mtl/mtl_ext.rs
@@ -11,6 +11,7 @@ pub fn generate_mipmaps(command_queue: &metal::CommandQueueRef, tex: &metal::Tex
 use imgref::ImgVec;
 use rgb::ComponentBytes;
 use rgb::RGBA8;
+use metalgear::GPUVec;
 
 pub trait MtlTextureExt {
     fn save(&self) -> ImgVec<RGBA8>;
@@ -105,3 +106,127 @@ impl MtlTextureExt for metal::TextureRef {
 //         self.set_fragment_bytes(index, std::mem::size_of::<T>() as u64, ptr as *const _)
 //     }
 // }
+
+
+pub trait GPUVecExt {
+    fn extend_with_triange_fan_indices_cw(&mut self, start: u32, count: u32) -> usize;
+}
+
+impl GPUVecExt for GPUVec<u32> {
+    /// Creates an indidex buffer which can be used to "fake" triangle fans
+    /// Based on pathfinder.
+    /// https://www.gamedev.net/forums/topic/643945-how-to-generate-a-triangle-fan-index-list-for-a-circle-shape/
+
+    fn extend_with_triange_fan_indices_cw(&mut self, start: u32, count: u32) -> usize {
+        let mut added = 0;
+        for index in 1..(count - 1) {
+            self.extend_from_slice(&[start, start + index, start + index + 1]);
+            added += 3;
+        }
+
+        added
+    }
+}
+
+fn triangle_fan_indices_cw(start: u32, len: u32) -> Vec<u32> {
+    let mut indices: Vec<u32> = vec![];
+    for index in 1..(len - 1) {
+        indices.extend_from_slice(&[start, start + index, start + index + 1]);
+    }
+
+    indices
+}
+fn triangle_fan_indices_ccw(start: u32, len: u32) -> Vec<u32> {
+    let mut indices: Vec<u32> = vec![];
+    for index in 1..(len - 1) {
+        indices.extend_from_slice(&[start, start + index + 1, start + index]);
+    }
+
+    indices
+}
+
+
+// from https://github.com/OpenSmalltalk/opensmalltalk-vm/blob/4ee8bb6e7960e5776558f0baca10daee7ec5d653/platforms/iOS/plugins/B3DAcceleratorPlugin/sqMetalRenderer.m#L718
+// unsigned int triangleCount = vertexCount - 2;
+// unsigned int renderIndexCount = triangleCount*3;
+// id<MTLBuffer> indexBuffer = [device newBufferWithLength: renderIndexCount*4 options: MTLResourceStorageModeManaged];
+
+// // Set the triangle fan indices.
+// unsigned int *destIndices = (unsigned int *)indexBuffer.contents;
+// for(unsigned int i = 2; i < vertexCount; ++i) {
+//     destIndices[0] = 0;
+//     destIndices[1] = i - 1;
+//     destIndices[2] = i;
+//     destIndices += 3;
+// }
+
+fn triangle_fan_indices2(device: &metal::DeviceRef, start: u32, len: u32) -> GPUVec<u32> {
+    let triangle_len = len - 2;
+    let index_len = 3 * triangle_len;
+    let mut vec = GPUVec::<u32>::with_capacity(device, index_len as usize);
+
+    // let mut indices: Vec<u32> = vec![];
+    for index in 2..(len) {
+        vec.extend_from_slice(&[start, start + index - 1, start + index]);
+    }
+
+    vec
+}
+
+// fn triangle_fan_indices3(
+//     start: u32,
+//     len: u32
+// ) -> Vec<u32> {
+//     let triangle_len = len - 2;
+//     let index_len = 3 * triangle_len;
+//     let mut vec = Vec::<u32>::with_capacity(index_len as);
+
+//     // let mut indices: Vec<u32> = vec![];
+//     for index in 2..(len) {
+//         vec.extend_from_slice(&[start, start + index - 1, start + index]);
+//     }
+
+//     vec
+// }
+/// expects buffer to be able to allocate vertices
+fn triangle_fan_indices_ext(start: u32, len: usize, buf: &mut GPUVec<u32>) {
+    // let mut indices: Vec<u32> = vec![];
+    let invariant = buf.capacity();
+    // for index in start..(start + len as u32 - 1) {
+    //     buf.extend_from_slice(&[start, index, index + 1]);
+    // }
+
+    for index in start..(start + len as u32 - 1) {
+        buf.extend_from_slice(&[start, index, index + 1]);
+    }
+
+    assert!(invariant == buf.capacity());
+}
+
+#[cfg(test)]
+mod tests {
+
+    use metalgear::GPUVec;
+    use super::GPUVecExt;
+    // use super::{}
+    // use metalgear::GPUVec;
+    // use super::GPUVecExt;
+
+    // #[test]
+    // fn test_triangle_fan_indices() {
+
+    // }
+
+    #[test]
+    fn test_triangle_fan_indices_cw() {
+        let expected1: Vec<u32> = vec![0, 1, 2, 0, 2, 3, 0, 3, 4];
+        let mut result1 = GPUVec::<u32>::new();
+        result1.extend_with_triange_fan_indices_cw(0, 5);
+        assert!(expected1[..] == result1[..]);
+
+        let expected2: Vec<u32> = vec![2, 3, 4, 2, 4, 5, 2, 5, 6];
+        let mut result2 = GPUVec::<u32>::new();
+        result2.extend_with_triange_fan_indices_cw(2, 5);
+        assert!(expected2[..] == result2[..]);
+    }
+}
